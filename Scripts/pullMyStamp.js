6ba8090e52560bf5c7318cc547d760e0b75e3885
Aman0jaku <53261863+Aman0jaku@users.noreply.github.com>
2020-05-05 23:41:27 +0900

    fix 05/05

---
+++ Scripts/pullMyStamp.js
@@ -0,0 +1,30 @@
+//myStampに登録したものを持ってくる
+//結局fileNameで検索かけてデータを引っ張るのはアプリサイドやけど
+module.exports = function(req, res){
+  //送られてきたデータを取得
+  var userId = req.body.userId;
+
+  //サブクラスの作成
+  var NCMB = require('ncmb');
+
+  //ARty用
+  var applicationKey = 'a8b68d30b39077b903e6eb7f9496b55bc134b5b6718140d1bb3b0e1a323ec4a7';
+  var clientKey = '437b274dfbd4347193a3e2e33828ca475c77e734288f2cd793da1397394b0fd6';
+
+  //インスタンスの生成
+  var ncmb = new NCMB(applicationKey, clientKey);
+
+  var myStamp = ncmb.DataStore("MyStamp");
+
+  myStamp.equalTo("userId", userId)
+         .fetchAll()
+         .then(function(results){
+           res.status(200)
+              .json(results);
+         })
+         .catch(function(err){
+           res.status(500)
+              .send("myStamp fetch error : " + err);
+         });
+
+}
---
+++ Scripts/pushMyView.js
@@ -0,0 +1,37 @@
+//numberOfViewsを更新する
+module.exports = function(req, res){
+  //送られてきたデータを取得
+  var stickId = req.body.stickId;
+
+  //サブクラスの作成
+  var NCMB = require('ncmb');
+
+  //ARty用
+  var applicationKey = 'a8b68d30b39077b903e6eb7f9496b55bc134b5b6718140d1bb3b0e1a323ec4a7';
+  var clientKey = '437b274dfbd4347193a3e2e33828ca475c77e734288f2cd793da1397394b0fd6';
+
+  //インスタンスの生成
+  var ncmb = new NCMB(applicationKey, clientKey);
+
+  var stick = ncmb.DataStore('Stick');
+
+  stick.equalTo("objectId", stickId)
+       .fetch()
+       .then(function(result){
+         result.setIncrement("numberOfViews", 1)
+               .update()
+               .then(function(res){
+                 res.status(200)
+                    .send("stick update success");
+               })
+               .catch(function(err){
+                 res.status(500)
+                    .send("stick update error : " + err);
+               });
+       })
+       .catch(function(err){
+         res.status(500)
+            .send("stick fetch error : " + err);
+       });
+
+}
